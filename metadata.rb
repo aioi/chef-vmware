a1cf462c7cf81b7b6669782b55a627856a37ea8f
Alexander Wenzowski <alex@ent.io>
2013-04-25 21:42:06 -0400

    bump version to 0.04

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "nacer.laradji@gmail.com"
 license          "Apache 2.0"
 description      "Installs vmware tools"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.3"
+version          "0.0.4"
 supports          "ubuntu", ">= 10.04"
