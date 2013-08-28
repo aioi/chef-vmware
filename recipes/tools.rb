3cc85c80393d703f8be09a8512718e69450d4cef
Olivier Brisse <ouranos@donarproject.org>
2013-08-28 15:19:48 +1000

    Add the key via the LWRP instead of a command

--- recipes/tools.rb
+++ recipes/tools.rb
@@ -10,17 +10,12 @@
 
 if node.virtualization.system == 'vmware'
 
-  execute 'add_vmware_repo_key' do
-    command 'wget http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub -q -O- | apt-key add -'
-    action :nothing
-  end
-
   apt_repository 'vmware-tools' do
     uri "http://packages.vmware.com/tools/esx/#{node.vmware.esx.tools.version}/ubuntu"
     distribution node['lsb']['codename']
     components ['main']
+    key 'http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub'
     action :add
-    notifies :run, resources('execute[add_vmware_repo_key]'), :immediately
   end
 
   package 'vmware-tools-esx-nox' do
