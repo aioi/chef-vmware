f50bde4f8b4c6f0290d83a97f71b469311d8a255
Olivier Brisse <ouranos@donarproject.org>
2013-08-28 12:10:01 +1000

    Convert readme to markdown and add changelog (FC012)

---
+++ README.md
@@ -0,0 +1,13 @@
+# DESCRIPTION:
+
+# REQUIREMENTS:
+
+# ATTRIBUTES:
+  Attribute [:vmware][:esx][:tools][:version] can be set for other vmware guest tools version
+
+
+# USAGE:
+  Simply include "recipe[vmware::tools]" on your run_list
+
+# TODO:
+    - Add support for other platform
--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ maintainer       "Efactures"
 maintainer_email "nacer.laradji@gmail.com"
 license          "Apache 2.0"
 description      "Installs vmware tools"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.4"
 supports          "ubuntu", ">= 10.04"
